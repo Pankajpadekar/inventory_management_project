225bb69aefb99583fc202d905cbfd010ab44a002
Pankaj Padekar <padekarpankaj@gmail.com>
2024-10-01 20:14:02 +0530

    created unit test

--- inventory_app/tests.py
+++ inventory_app/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from .models import Item
 
-# Create your tests here.
+class ItemTest(APITestCase):
+    def test_item(self):
+        url = reverse('item-create')
+        data = {'name' : 'Lenovo', 'description' : 'Laptop manufacturing Company'}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
